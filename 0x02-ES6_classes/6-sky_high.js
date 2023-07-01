76e8dc4f3a580ea2a3993b544ed3fcaf7bb045d0
Sadiya <Shahidakilgori@gmail.com>
2023-06-30 17:01:02 -0700

    0

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,23 +1,23 @@
-import Building from './5-building.js';
+import Building from './5-building';
 
-class SkyHighBuilding extends Building {
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this._sqft = sqft;
-    this._floors = floors;
-  }
-
-  get sqft() {
-    return this._sqft;
+    this.floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw TypeError('Floors must be a number');
+    }
+    this._floors = newFloors;
+  }
+
   evacuationWarningMessage() {
     return `Evacuate slowly the ${this._floors} floors`;
   }
 }
-
-export default SkyHighBuilding;
